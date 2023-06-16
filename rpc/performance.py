a0767249ac76c141d08c314830d980e3a75390ab
Yury Miklashevich1 <yury_miklashevich1@epam.com>
2023-06-16 15:36:05 +0300

    Support new integrations

--- rpc/performance.py
+++ rpc/performance.py
@@ -12,20 +12,19 @@ class RPC:
 
     @web.rpc(f'backend_performance_test_create_integration_validate_{integration_name}')
     @rpc_tools.wrap_exceptions(ValidationError)
-    def backend_performance_test_create_integration_validate(self, data: dict,
+    def backend_performance_test_create_integration_validate(self, data: dict, project_id: int,
             pd_kwargs: Optional[dict] = None, **kwargs
     ) -> dict:
         if not pd_kwargs:
             pd_kwargs = {}
-        log.info(f"before validation {data=}")
-        integration = self.context.rpc_manager.call.integrations_get_by_id(data["id"])
+        integration = self.context.rpc_manager.call.integrations_get_by_id(project_id, data["id"])
         pd_object = PerformanceBackendTestModel(**{**integration.settings, **data})
         pd_object.k8s_token = pd_object.k8s_token.value
         return pd_object.dict(**pd_kwargs)
 
     @web.rpc(f'backend_performance_execution_json_config_{integration_name}')
     @rpc_tools.wrap_exceptions(RuntimeError)
-    def backend_make_execution_json_config(self, integration_data: dict) -> dict:
+    def backend_make_execution_json_config(self, integration_data: dict, project_id: int) -> dict:
         """ Prepare execution_json for this integration """
         return integration_data
 
@@ -37,13 +36,14 @@ class RPC:
     ) -> dict:
         if not pd_kwargs:
             pd_kwargs = {}
-        integration = self.context.rpc_manager.call.integrations_get_by_id(data["id"])
+        project_id = kwargs.get('project_id')
+        integration = self.context.rpc_manager.call.integrations_get_by_id(project_id, data["id"])
         pd_object = PerformanceUiTestModel(**{**integration.settings, **data})
         pd_object.k8s_token = pd_object.k8s_token.value
         return pd_object.dict(**pd_kwargs)
 
     @web.rpc(f'ui_performance_execution_json_config_{integration_name}')
     @rpc_tools.wrap_exceptions(RuntimeError)
-    def ui_make_execution_json_config(self, integration_data: dict) -> dict:
+    def ui_make_execution_json_config(self, integration_data: dict, project_id: int) -> dict:
         """ Prepare execution_json for this integration """
         return integration_data
